e9886883e4c3fd50a87203fc744dc838f7c10c45
marinaher <b-her@live.com>
2016-09-03 13:32:19 -0500

    added binary search tree

--- Nodes/BinarySearchTree.cs
+++ Nodes/BinarySearchTree.cs
@@ -1,34 +1,114 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Sockets;
 
 namespace Nodes
 {
-    public class BinarySearchTree<T> : IComparable<T>
+    class BinarySearchTree<TKey, TValue> : IEnumerable<Nodes<TKey, TValue>> where TKey : IComparable<TKey>
     {
-        //Nodes<T> = Nodes;
+        Dictionary<string, Socket> Clients = new Dictionary<string, Socket>();
 
-        public BinarySearchTree(T item)
-        {
-            
-        }
-        public void Search(T item)
-        {
+        private Nodes<TKey, TValue> _root;
 
-        }
-        public void Add(T item)
+        public bool Search(TKey key, out TValue value)
         {
-            
+            Nodes<TKey, TValue> node = _root;
+
+            while (node != null)
+            {
+                if (key.CompareTo(node.Key) < 0)
+                {
+                    node = node.Left;
+                }
+                else if (key.CompareTo(node.Key) > 0)
+                {
+                    node = node.Right;
+                }
+                else
+                {
+                    value = node.Value;
+                    return true;
+                }
+            }
+
+            value = default(TValue);
+
+            return false;
         }
-        public void Remove(T item)
+
+        public void Add(TKey key, TValue value)
         {
-            
+            if (_root == null)
+            {
+                _root = new Nodes<TKey, TValue>(key, value);
+            }
+            else
+            {
+                Nodes<TKey, TValue> node = _root;
+
+                while (node != null)
+                {
+                    int comaprisonResult = node.Key.CompareTo(key);
+
+                    if (comaprisonResult < 0)
+                    {
+                        Nodes<TKey, TValue> left = node.Left;
+
+                        if (left == null)
+                        {
+                            node.Left = new Nodes<TKey, TValue>(key, value, node);
+                            return;
+                        }
+                        node = left;
+                    }
+                    else if (comaprisonResult > 0)
+                    {
+                        Nodes<TKey, TValue> right = node.Right;
+
+                        if (right == null)
+                        {
+                            node.Right = new Nodes<TKey, TValue>(key, value, node);
+                            return;
+                        }
+                        node = right;
+                    }
+                    else
+                    {
+                        node.Key = key;
+                        node.Value = value;
+                        return;
+                    }
+                }
+            }
         }
-        public int CompareTo(T item)
+        private static void Replace(Nodes<TKey, TValue> target, Nodes<TKey, TValue> source)
         {
-            throw new NotImplementedException();
+            Nodes<TKey, TValue> left = source.Left;
+            Nodes<TKey, TValue> right = source.Right;
+
+            target.Key = source.Key;
+            target.Value = source.Value;
+            target.Left = left;
+            target.Right = right;
+
+            if (left != null)
+            {
+                left.Parent = target;
+            }
+
+            if (right != null)
+            {
+                right.Parent = target;
+            }
+        }
+        IEnumerator<Nodes<TKey, TValue>> IEnumerable<Nodes<TKey, TValue>>.GetEnumerator()
+        {
+            return _root.GetEnumerator();
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return ((IEnumerable)_root).GetEnumerator();
         }
     }
 }
